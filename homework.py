639bcefce701ca69a15db30be29635a6f7803c47
MPolsh <m@polshkov.ru>
2023-05-07 00:23:16 +0500

    Начало выполнения задания

--- homework.py
+++ homework.py
@@ -1,11 +1,16 @@
-...
+import os
+import requests
+
+from dotenv import load_dotenv
+
+from exceptions import LostEnvVarError
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -20,31 +25,38 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
-    ...
+    if not PRACTICUM_TOKEN or not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
+        raise LostEnvVarError()
+
 
 
 def send_message(bot, message):
-    ...
+    bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
 def get_api_answer(timestamp):
-    ...
+    homework_statuses = requests.get(
+        ENDPOINT,
+        headers=HEADERS,
+        params=timestamp
+    )
+    return homework_statuses.json()
 
 
 def check_response(response):
-    ...
+    
 
 
 def parse_status(homework):
-    ...
-
+    last_homework = homework['homeworks'][0]
+    homework_name = last_homework['homework_name']
+    verdict = HOMEWORK_VERDICTS[last_homework['status']]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
     """Основная логика работы бота."""
-
-    ...
+    check_tokens()
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
